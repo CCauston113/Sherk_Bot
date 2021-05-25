f8b28e5acb9b85ec641474465ba39d7c37570a36
Charlotte Causton <65340340+CCauston113@users.noreply.github.com>
2021-05-25 16:18:53 +0100

    Add responses
    Sherk now responds to messages containing "good sherk" and "sherk"

--- bot.py
+++ bot.py
@@ -9,8 +9,28 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+vocab = { #put here your vocabulary, in the format "keyword":"answer",
+"good sherk": "<:sherk_the_third:810909639909572640> <:axopogtl:814165531019182151>",
+"sherk": "<:sherk:807344108204326982> <:sherk_char:807344973929513010> <:sherk_the_third:810909639909572640>"
+
+}
+
+#Alert for discord connect
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+#For keyword -> message
+@client.event
+async def on_message(message):
+	channel = message.channel
+	if message.author == client.user: #no replying to own messages, ever (first rule of Discord bots)
+		return
+	
+	for keyword in vocab: #check for each keyword: do we know it?
+		if keyword in message.content.lower():
+			await channel.send(vocab[keyword]) #if yes, send the answer
+			break;
+
 
 client.run(TOKEN)
